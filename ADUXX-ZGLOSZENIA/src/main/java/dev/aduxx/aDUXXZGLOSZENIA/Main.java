43763479adcdf41d50f4e573d039a7c86a2120a4
Aduxx <adrian.mical26@gmail.com>
2025-06-16 10:54:21 +0200

    Add files via upload

---
+++ ADUXX-ZGLOSZENIA/src/main/java/dev/aduxx/aDUXXZGLOSZENIA/Main.java
@@ -0,0 +1,24 @@
+package dev.aduxx.aDUXXZGLOSZENIA;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import reports.BanListener;
+import reports.ReportTabCompleter;
+
+public class Main extends JavaPlugin {
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        getCommand("zglos").setExecutor(new ReportCommand());
+        getServer().getPluginManager().registerEvents(new ReportGui(), this);
+        getServer().getPluginManager().registerEvents(new ReportListener(), this);
+        getServer().getPluginManager().registerEvents(new BanListener(),this);
+        this.getCommand("zglos").setTabCompleter(new ReportTabCompleter());
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
